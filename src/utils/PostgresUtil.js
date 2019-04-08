a99a6d5a68c941ab762c31906c777916f89b8a6e
midgriffin <43070418+midgriffin@users.noreply.github.com>
2019-04-08 18:59:16 -0300

    final push

--- src/utils/PostgresUtil.js
+++ src/utils/PostgresUtil.js
@@ -1,8 +1,13 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
+  // These 2 lines are used for the online version
   connectionString: process.env.DATABASE_URL,
-  // ssl: true,
+  ssl: true
+  // These 2 lines are used for the local version
+  //password: "student",
+  //database: "postgres"
+
 })
 
 module.exports = {
